c2ab39252de3fdc0993935ecc0d3dbf8d9a573c3
Haythem <heythem.94@gmail.com>
2023-11-15 23:50:14 +0000

    betty styling main

--- main.c
+++ main.c
@@ -4,29 +4,31 @@
  * main - Entry point of the program
  * Return: EXIT_SUCCESS on successful execution
  */
+
 int main()
 {
-    loop();
-    return EXIT_SUCCESS;
+	loop();
+	return (EXIT_SUCCESS);
 }
 
 /**
  * loop - Repeatedly prompts the user
  */
-void loop()
+
+void loop(void)
 {
-    char *line;
-    char **args;
-    int status;
+	char *line;
+	char **args;
+	int status;
 
-    do {
-        printf("$ ");
+	do 
+	{
+	printf("$ ");
 	fflush(stdout);
-        line = read_line();
-        args = split_line(line);
-        status = execute(args);
-
-        free(line);
-        free(args);
-    } while (status);
+	line = read_line();
+	args = split_line(line);
+	status = execute(args);
+	free(line);
+	free(args);
+	} while (status);
 }
